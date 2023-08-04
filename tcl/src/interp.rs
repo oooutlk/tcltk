ef48f5f11992a9922b0a9ea68573e27885caddbd
Ben Burwell <ben.burwell@flightaware.com>
2023-08-04 13:35:48 -0400

    Add package_provide method to Interpreter
    This exposes a hook for the Tcl_PkgProvide C API, and allows callers to
    expose Rust functionality as a Tcl package.

--- tcl/src/interp.rs
+++ tcl/src/interp.rs
@@ -571,6 +571,18 @@ impl Interp {
         self.eval(( "package", "require", name ))
     }
 
+    /// This is equivalent to calling "package provide" with the specified package name and
+    /// version.
+    pub unsafe fn package_provide(&self, name: &str, version: &str) -> c_int {
+        let name = CString::new(name).expect("Tcl package name should be CString.");
+        let version = CString::new(version).expect("Tcl package version should be CString.");
+        clib::Tcl_PkgProvide(
+            self.as_ptr(),
+            name.as_c_str().as_ptr(),
+            version.as_c_str().as_ptr(),
+        )
+    }
+
     /// This command takes the contents of the specified file or resource and passes it
     /// to the Tcl interpreter as a text script. The return value from source is the
     /// return value of the last command executed in the script. If an error occurs in
@@ -690,5 +702,23 @@ mod tests {
         if let Err( err ) = interp.eval(( "badcommand", "or filename" )) {
             println!( "err:{:?}", err );
         }
+    }
+
+    #[test]
+    fn package_provide() {
+        let interp = Interpreter::new().unwrap();
+        unsafe {
+            assert_eq!(
+                interp.package_provide("mypackage", "1.2.3"),
+                i32::try_from(clib::TCL_OK).unwrap()
+            );
+        }
+        assert_eq!(
+            interp
+                .eval("package require mypackage")
+                .unwrap()
+                .get_string(),
+            "1.2.3".to_string()
+        );
     }
 }
